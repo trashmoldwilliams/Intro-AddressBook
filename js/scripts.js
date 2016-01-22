de78cacfab5553f71e8b1e95755b01e7b498c5e1
Mike Goren <mgoren@mortalwombat.net>
2016-01-21 22:35:03 -0800

    add front-end for adding new contacts

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,18 @@ function Contact(firstName,lastName) {
 Contact.prototype.fullName = function() {
   return this.firstName + " " + this.lastName;
 }
+
+$(document).ready(function() {
+  $("form#new-contact").submit(function(event) {
+    event.preventDefault();
+
+    var inputtedFirstName = $("input#new-first-name").val();
+    var inputtedLastName = $("input#new-last-name").val();
+    var newContact = new Contact(inputtedFirstName, inputtedLastName);
+
+    $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
+
+    $("input#new-first-name").val("");
+    $("input#new-last-name").val("");
+  });
+});
